e2c01258a059fb29758e1d97770fcb5b3be06095
Balla.Mate <balla.mate21k@egressy.info>
2024-02-13 10:53:19 +0100

    balla

--- Balla Máté/py/py2.py
+++ Balla Máté/py/py2.py
@@ -488,3 +488,107 @@ if list:
     print(f'A legnagyobb szó a listában: {legnagyobb}')
 else:
     print("Nem adtál meg szavakat.")
+
+
+
+def koszont():
+	      print('Üdv a fedélzeten!')
+
+koszont()
+  
+
+def koszont_nevvel(nev):
+	      print('Szia '+ nev +', üdv a fedélzeten!')
+
+koszont_nevvel('Ádám')
+  
+
+def koszont_ket_nevvel(nev1, nev2):
+	      print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+
+koszont_ket_nevvel('Nóra', 'Ádám')
+    
+
+def osszead(x, y):
+	    eredmeny = x + y
+print('A két szám összege: ', eredmeny)
+
+
+osszead(10, 9)
+osszead(5+5, 5+4)
+
+a = 10
+b = 9
+osszead(a, b) 
+
+def osszegzo(list):
+	osszesen = 0
+for szam in list:
+  osszesen = osszesen + szam
+print('A listában lévő számok összege: ', osszesen)
+
+
+szamok = [3, 5, 19, 11, 17, 1]
+osszegzo(szamok)
+    
+    
+
+
+
+
+def kepernyore_ir():
+        lokalis_valtozo = 'alma'
+        print(lokalis_valtozo)
+        print(globalis_valtozo)
+  
+  
+globalis_valtozo = 'gyümölcs'
+kepernyore_ir()
+  
+print(globalis_valtozo)
+    # a lokalis_valtozo az eljáráson KÍVŰL nem elérhető !!!
+
+        # Függvény definíciója
+def festek_kalkulator(x, y):
+            """
+            Kiszámolja az adott falfelület festéséhez
+            szükséges festék mennyiségét
+            """
+            t = x * y
+            l = t * 0.13
+            return l
+    
+    
+        # Függvény hívása
+liter = festek_kalkulator(5, 2)
+    
+        # A függvény hívása lehet egy kifejezés része is
+ar = festek_kalkulator(5, 2) * 700
+    
+
+def szamolo(a, b, c=100):     
+  return (a+b)*c
+  
+    
+        # alapértelmezett paraméter használata
+print(szamolo(1, 7))
+    
+        # alapértelmezett paraméter felülírása
+print(szamolo(1, 7, 10000))
+    
+        # név szerinti paraméter átadás
+print(szamolo(c=10000, a=1, b=7))
+    
+
+def legnagyobb_kereso(x, *args):
+             
+              legnagyobb = x
+              for szam in args:
+                    if szam > legnagyobb:
+                          legnagyobb = szam
+              return legnagyobb
+    
+    
+print(legnagyobb_kereso(1, 19, 11, 7, 17))
+    
+    
