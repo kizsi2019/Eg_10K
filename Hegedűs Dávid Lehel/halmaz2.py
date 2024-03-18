d7ba7e61b8f14a841fb6994e4f4c129b227610de
HegDavid <hegedus.david.lehel.21k@egressy.info>
2024-03-18 08:15:08 +0100

    minden

---
+++ Hegedűs Dávid Lehel/halmaz2.py
@@ -0,0 +1,12 @@
+
+reggeli = {'víz', 'tea', 'vaj', 'pirítós'}
+ebed = {'víz', 'halászlé', 'kenyér'}
+
+
+print(reggeli & ebed)
+
+print(reggeli | ebed)
+print(reggeli - ebed)
+print(reggeli ^ ebed)
+    
+  
